200b35a4c1d058df405c8d8d6aabd5a4e996e27c
Student <student@bluc02kl120drv6.local>
2016-02-16 12:22:47 -0600

    Allow for entries to be added.

--- CollegeProfieBuilder2/ViewController.swift
+++ CollegeProfieBuilder2/ViewController.swift
@@ -27,6 +27,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             tableView.reloadData()
         }
     }
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add Colleges", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Add Colleges Here"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel , handler: nil )
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(collegeTextField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
