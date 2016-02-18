e149162df4d31a5f51b84f1e9a83de52a39b3da2
Student <student@bluc02kl120drv6.local>
2016-02-18 12:21:03 -0600

    Allow for entries to be re-ordered.

--- CollegeProfieBuilder2/ViewController.swift
+++ CollegeProfieBuilder2/ViewController.swift
@@ -9,30 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     @IBOutlet weak var tableView: UITableView!
     var colleges = ["University of Illinois", " HarperCollege", "University of Wisconsin-Madison"]
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
         cell.textLabel?.text = colleges[indexPath.row]
         return cell
     }
+    
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             colleges.removeAtIndex(indexPath.row)
             tableView.reloadData()
         }
     }
+    
     @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add Colleges", message: nil, preferredStyle: .Alert)
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
             textField.placeholder = "Add Colleges Here"
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel , handler: nil )
         let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
             let collegeTextField = alert.textFields![0] as UITextField
@@ -43,16 +45,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-
-
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexpath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let college = colleges[sourceIndexPath.row]
+            colleges.removeAtIndex(sourceIndexPath.row)
+            colleges.insert(college, atIndex: destinationIndexPath.row)
+    
+        }
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBAction func onTappedEditButton(sender: UIBarButtonItem) {
+        editButton.tag = 0
+        if sender.tag == 0 {
+            tableView.editing = true
+            sender.tag = 1
+            
+        }
+        else{
+            tableView.editing = false
+            sender.tag = 0
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
- 
-
-
+    
+    
+    
+    
 }
 
